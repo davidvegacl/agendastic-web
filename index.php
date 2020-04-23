defba69a4006550d991ce663dc4349f73dcc23bd
David Vega <dvega@prosys.cl>
2020-04-23 00:46:30 -0400

    UPD: primera version de la web

---
+++ index.php
@@ -0,0 +1,145 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Agendastic es un sistema de agendamiento en línea para centros médicos, profesionales independientes, barberías, salones de belleza, servicios mecánicos, entre otros.">
+    <meta name="author" content="David Vega Regollo - david@izarus.cl">
+    <title>Agendastic - Agendamiento en línea</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png" sizes="180x180">
+    <link rel="icon" href="img/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="img/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="manifest" href="img/site.webmanifest">
+    <link rel="mask-icon" href="img/safari-pinned-tab.svg" color="#313f49">
+    <link rel="icon" href="img/favicon.ico">
+    <meta name="msapplication-config" content="img/browserconfig.xml">
+    <meta name="theme-color" content="#313f49">
+
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+
+    <nav class="site-header sticky-top py-1">
+        <div class="container d-flex flex-column flex-md-row justify-content-between">
+            <a class="py-1" href="/">
+                <img class="brand" src="img/agendastic-h.png" alt="Agendastic">
+            </a>
+            <div class="slogan">Agendamiento en línea</div>
+        </div>
+    </nav>
+
+    <div class="main-banner position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center">
+        <div class="col-md-5 p-lg-5 mx-auto my-5">
+            <h1 class="display-4 font-weight-normal">Agendamiento en línea</h1>
+            <p class="lead font-weight-normal texto">
+                Si tienes un centro de salud, peluquería, barbería, salón de belleza, o cualquier negocio
+                donde debas organizar tu agenda, <b>incluso si has tenido que atender online</b>,
+                entonces <b>Agendastic</b> es para ti.
+                <br>
+                <br>
+                Más info en agendastic@davidvega.cl
+            </p>
+            <!-- <a class="btn btn-success" href="#">Pronto más información</a> -->
+        </div>
+    </div>
+
+    <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+        <div class="bg-brand mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+            <div class="my-3 py-3">
+                <h2 class="display-5">Simples 4 pasos para agendar</h2>
+                <p class="lead">Proveedor, servicio, fecha y hora, datos personales y confirmar.</p>
+            </div>
+            <div class="shadow-sm mx-auto overflow-hidden" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;">
+                <img src="img/screen1.png" alt="" class="img-fluid">
+            </div>
+        </div>
+        <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div class="my-3 p-3">
+                <h2 class="display-5">Bloques horarios por servicio</h2>
+                <p class="lead">Define la duración de los bloques horarios y ajustalos durante tu jornada.</p>
+            </div>
+            <div class="shadow-sm mx-auto overflow-hidden" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;">
+                <img src="img/screen2.png" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+
+    <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+        <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div class="my-3 p-3">
+                <h2 class="display-5">Notificaciones por e-mail</h2>
+                <p class="lead">Tanto el cliente como el proveedor recibirán una notificación de la cita.</p>
+            </div>
+            <div class="shadow-sm mx-auto overflow-hidden" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;">
+                <img src="img/screen3.png" alt="" class="img-fluid">
+            </div>
+        </div>
+        <div class="bg-brand-green mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+            <div class="my-3 py-3">
+                <h2 class="display-5">Tu calendario a la vista</h2>
+                <p class="lead">Todas tus citas en una sola pantalla.</p>
+            </div>
+            <div class="shadow-sm mx-auto overflow-hidden" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;">
+                <img src="img/screen4.png" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+
+    <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+        <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div class="my-3 p-3">
+                <h2 class="display-5">Tu directorio de clientes</h2>
+                <p class="lead">Cada vez que alguien agende una cita, tendrás su información a mano para contacto.</p>
+            </div>
+            <div class="shadow-sm mx-auto overflow-hidden" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;">
+                <img src="img/screen5.png" alt="" class="img-fluid">
+            </div>
+        </div>
+        <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div class="my-3 py-3">
+                <h2 class="display-5">Horario a medida</h2>
+                <p class="lead">La dispoibilidad horaria en un solo lugar, por día y con períodos de descanso.</p>
+            </div>
+            <div class="shadow-sm mx-auto overflow-hidden" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;">
+                <img src="img/screen6.png" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+
+
+    <section class="d-block my-5 container">
+        <p class="py-3 h2 font-weight-normal text-center">
+            ¿Te interesa? Escribe ahora a <a class="font-weight-bold text-success" href="mailto:agendastic@davidvega.cl">agendastic@davidvega.cl</a> para obtener más información.
+        </p>
+    </section>
+
+
+    <footer class="container py-5">
+        <div class="row">
+            <div class="col-12 col-md">
+                <img class="img-fluid mb-4" src="img/agendastic-h.png" alt="Agendastic">
+                <small class="d-block mb-3 text-muted">&copy; 2020</small>
+                <div class="small"><a class="text-muted" href="https://davidvega.cl">https://davidvega.cl</a></div>
+            </div>
+            <div class="col-6 col-md">
+                
+            </div>
+            <div class="col-6 col-md">
+
+            </div>
+            <div class="col-6 col-md">
+            
+            </div>
+            <div class="col-6 col-md">
+            
+            </div>
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
